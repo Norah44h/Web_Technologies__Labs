3b050f41a21dac5d1fe4ae3ed19da96fb366be75
Norah <412205566@qu.edu.sa>
2026-03-04 02:29:03 +0300

    Finalize Lab 4: updated layouts and documentation

--- Web_Tech_Project/apps/bookmodule/urls.py
+++ Web_Tech_Project/apps/bookmodule/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('index2/', views.index2, name="index2"),
+    #path('index2/', views.index2, name="index2"),
+    path('', views.index, name="books.index"),
+    path('list_books/', views.list_books, name="books.list_books"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
 ]
