208d180a45800ea87ee432a36015adc36c306249
Norah <412205566@qu.edu.sa>
2026-04-11 16:43:04 +0300

    Lab 7: 80 سطر بيانات مزيفة + اضافة خاصية الفلاتر البسيطة والمتقدمة

--- Web_Tech_Project/apps/bookmodule/urls.py
+++ Web_Tech_Project/apps/bookmodule/urls.py
@@ -4,11 +4,14 @@ from . import views
 urlpatterns = [
     #path('index2/', views.index2, name="index2"),
     path('', views.index, name="books.index"),
-    path('list_books/', views.list_books, name="books.list_books"),
+    #path('list_books/', views.list_all_books, name="list_all_books"),
     path('aboutus/', views.aboutus, name="books.aboutus"),
     path('html5/links', views.links_view, name='books.html5_links'),
     path('html5/text/formatting', views.formatting_view, name='books.html5_formatting'),
     path('html5/listing', views.listing_view, name='books.html5_listing'),
     path('html5/tables', views.tables_view, name='books.html5_tables'),
     path('search', views.search_view, name='books.search'),
+    path('simple/query', views.simple_query, name='books.simple_query'),
+    path('complex/query', views.complex_query, name='books.complex_query'),
+    path('list_books/', views.list_all_books, name="list_all_books"),
 ]
--- Web_Tech_Project/apps/bookmodule/views.py
+++ Web_Tech_Project/apps/bookmodule/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book
 
 
 #def index(request):  #http://127.0.0.1:8000/
@@ -12,19 +13,15 @@ from django.shortcuts import render
 #def index(request):
     #return render(request, "bookmodule/index.html")  #http://127.0.0.1:8000/
 
-def index(request):
-    name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html", {"name": name})  #http://127.0.0.1:8000?name=Norah
+#def index(request):
+    #name = request.GET.get("name") or "world!"
+    #return render(request, "bookmodule/index.html", {"name": name})  #http://127.0.0.1:8000?name=Norah
 
 def index2(request, val1 = 0):
     return HttpResponse("value1 = " + str(val1))
 
-
 def index(request):
     return render(request, "bookmodule/index.html")
-
-def list_books(request):
-    return render(request, 'bookmodule/list_books.html')
 
 def viewbook(request, bookId):
     return render(request, 'bookmodule/one_book.html')
@@ -44,57 +41,47 @@ def listing_view(request):
 def tables_view(request):
     return render(request, 'bookmodule/tables.html')
 
-def search_view(request):
-    return render(request, 'bookmodule/search.html')
-
-
-def list_books(request):
-    # جلب كل الكتب من الدالة التي أنشأناها يدوياً
-    all_books = __getBooksList()
-
-    # إرسال كل الكتب لملف list_books.html
-    return render(request, 'bookmodule/list_books.html', {'books': all_books})
-
-def __getBooksList():
-    return [
-        {'id': 1, 'title': 'تأملات يومية', 'author': 'نورة محمد', 'category': 'رواية'},
-        {'id': 2, 'title': 'ماذا لو فتح الباب', 'author': 'كاتب أدبي', 'category': 'رواية أدبية'},
-        {'id': 3, 'title': 'Silent Whispers', 'author': 'Unknown', 'category': 'فلسفة وتأملات'},
-        {'id': 4, 'title': 'Machines That Matter', 'author': 'Tech Expert', 'category': 'تطوير ذات'}
-    ]
-
+def list_all_books(request):
+    all_books = Book.objects.all()
+    return render(request, 'bookmodule/bookList.html', {'books': all_books})
 
 def search_view(request):
     if request.method == "POST":
-        # 1. استقبال الكلمة وتحويلها لحروف صغيرة (للبحث بالإنجليزية بدقة)
         string = request.POST.get('keyword').lower()
-
-        # 2. التأكد من الخيارات المختارة (العنوان أو المؤلف)
-        # لاحظي أن الأسماء 'option1' و 'option2' يجب أن تطابق الـ name في ملف search.html
         isTitle = request.POST.get('option1')
         isAuthor = request.POST.get('option2')
 
-        # 3. جلب قائمة كتبكِ الخاصة
-        books = __getBooksList()
+        books = Book.objects.all()
         newBooks = []
 
-        # 4. محرك البحث (الفلترة)
         for item in books:
             contained = False
-            # البحث في العنوان
-            if isTitle and string in item['title'].lower():
+            #  هنا استخدمنا item.title وليس ['title'] لأنها كائنات من الداتابيس
+            if isTitle and string in item.title.lower():
                 contained = True
-            # البحث في المؤلف (إذا لم يجد في العنوان أو إذا اختار المستخدم المؤلف أيضاً)
-            if not contained and isAuthor and string in item['author'].lower():
+            if not contained and isAuthor and string in item.author.lower():
                 contained = True
-
-            # إذا وجدنا تطابقاً، نضيف الكتاب لقائمة النتائج
             if contained:
                 newBooks.append(item)
 
-        # 5. إرسال النتائج لصفحة bookList.html لعرضها
-        return render(request, 'bookmodule/list_books.html', {'books': newBooks})
-
-    # إذا كان الطلب فتح الصفحة لأول مرة (GET)
+        return render(request, 'bookmodule/bookList.html', {'books': newBooks})
     return render(request, 'bookmodule/search.html')
+
+def simple_query(request):
+    # جلب الكتب التي يحتوي عنوانها على كلمة 'and' (تجاهل حالة الأحرف)
+    mybooks = Book.objects.filter(title__icontains='and')
+    return render(request, 'bookmodule/bookList.html', {'books': mybooks})
+
+def complex_query(request):
+    # الفلترة هنا دقيقة جداً حسب طلب اللاب
+    mybooks = Book.objects.filter(author__isnull=False)\
+                         .filter(title__icontains='d')\
+                         .filter(edition__gte=2)\
+                         .exclude(price__lte=100)
+    if len(mybooks) >= 1:
+        return render(request, 'bookmodule/bookList.html', {'books': mybooks})
+    else:
+        return render(request, 'bookmodule/index.html')
+
+
 
